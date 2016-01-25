ed9c1ee5c68599cbad44d1cb7bb08875fa6d3ebf
Wolfgang Faust <wolfgang42@git>
2016-01-24 23:43:13 -0500

    Minification: some whitespace removal

--- draw.min.c
+++ draw.min.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
 #define N getchar()
-char m[320][386][3];
-main(){puts("P6 386 320 255");
-	int c=N,i=0;
-	while(c--) {
+char m[320][386][3];main(){puts("P6 386 320 255");int c=N,i=0;while(c--){
 		int r=N,g=N,b=N,x=N*2,v=N*2,u=N*2,w=N*2,y;
 		for(;x<=v;x++){
 			for(y=u;y<=w;y++){
@@ -13,8 +9,5 @@ main(){puts("P6 386 320 255");
 			}
 		}
 	}
-	while(i<370560){
-		putchar(*((**m)+i++));
-	}
-	return 0;
+	while(i<370560){putchar(*((**m)+i++));}
 }
