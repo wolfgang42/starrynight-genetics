483386373e5aabe09b52bdf0b146c2ddd72852a8
Wolfgang Faust <wolfgang42@git>
2016-01-24 23:40:31 -0500

    Minification: Crunch variables and for loops

--- draw.min.c
+++ draw.min.c
@@ -4,12 +4,9 @@ char m[320][386][3];
 main(){puts("P6 386 320 255");
 	int c=N,i=0;
 	while(c--) {
-		char r=N,g=N,b=N;
-		
-		int startx=N*2,endx=N*2,starty=N*2,endy=N*2;
-		
-		for (int x = startx; x<=endx; x++) {
-			for (int y = starty; y<=endy; y++) {
+		int r=N,g=N,b=N,x=N*2,v=N*2,u=N*2,w=N*2,y;
+		for(;x<=v;x++){
+			for(y=u;y<=w;y++){
 				m[y][x][0]=r;
 				m[y][x][1]=g;
 				m[y][x][2]=b;
